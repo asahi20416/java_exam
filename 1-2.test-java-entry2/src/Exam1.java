20901fbe87514142a0612a2a2012e407ce322e89
城戸朝日 <rinuchan20416@gmail.com>
2026-01-16 11:10:41 +0900

    プログラミング入門2

---
+++ 1-2.test-java-entry2/src/Exam1.java
@@ -0,0 +1,16 @@
+public class Exam1 {
+    public static void main(String[] args) {
+        
+        int totalScore = 101;
+
+        if (totalScore == 100) {
+            System.out.println("満点です");
+        } else if (totalScore >= 80 && totalScore < 100) {
+            System.out.println("合格です");
+        } else if (totalScore >= 0 && totalScore < 80) {
+            System.out.println("追試です");
+        } else {
+            System.out.println("存在しない点数です");
+        }
+    }
+}
